9a3217b0aa0554da7375a399f137840b460daf40
Jan Reinsch <jan.reinsch@kardex.com>
2018-04-03 14:13:03 +0200

    ap / equals

--- src/main.js
+++ src/main.js
@@ -1,6 +1,24 @@
 var S = require("../node_modules/sanctuary");
 const R = require("../node_modules/ramda");
 
+console.log('')
+console.log('                                     SETOID                             ')
+console.log('')
+console.log("------------------------------  the equals method -----------------------------------------")
+console.log("-> Function")
+console.log('S.equals(x => x*x, x => x*x) =', S.equals(x => x*x, x => x*x))
+var fun = x => x*x;
+console.log("var fun = x => x*x; S.equals(fun, fun) =", S.equals(fun, fun));
+console.log("-> Object")
+console.log("S.equals({a:3},{a:3}) =", S.equals({a:3},{a:3}));
+console.log("-> Array")
+console.log("S.equals([1,2,3,4], [1,2,3,4]) =", S.equals([1,2,3,4],[1,2,3,4]));
+console.log("-> Number")
+console.log("S.equals(3, 4) =", S.equals(3, 4));
+console.log("can also be applied to boolean, string")
+console.log("-------------------------------------------------------------------------------------------")
+console.log('')
+
 console.log("S.of(S.Maybe, 42) =", S.of(S.Maybe, 42));
 console.log("S.map(x=>x*x, [1,2,3,4]) =", S.map(x=>x*x, [1,2,3,4]));
 console.log("S.toMaybe(null) =", S.toMaybe(null));
@@ -20,8 +38,7 @@ console.log('S.contramap(s => s.length, Math.sqrt)("Sanctuary") =', S.contramap(
 console.log('S.contramap(Math.sqrt, x => x + "abc")(9) =', S.contramap(Math.sqrt, x => x + "abc")(9));
 console.log('S.contramap(x => x, x => x)(42) =', S.contramap(x => x, x => x)(42));
 
-console.log("S.ap([x => x+1, x => x*x], [1,2,3,4]) =", S.ap([x => x+1, x => x*x], [1,2,3,4]));
-console.log('S.ap({a: x => S.parseInt(16)(x).value }, {a: "3", b: "4", c: "6"}) =', S.ap({a: x => S.parseInt(16)(x).value }, {a: "3", b: "4", c: "6"}));
+
 console.log('S.map(S.parseInt(16), {a: "3", b: "4", c: "6"}) =', S.map(S.parseInt(16), {a: "3", b: "4", c: "6"}));
 console.log('S.map(S.parseInt(16), {a: "3", b: "4", c: "X"}) =', S.map(S.parseInt(16), {a: "3", b: "4", c: "X"}));
 
@@ -59,8 +76,36 @@ console.log('S.isRight(q(0)) =', S.isRight(q(0)));
 console.log('S.map(x => x * x, S.Right(4)) =', S.map(x => x * x, S.Right(4)))
 console.log('S.map(x => x * x, S.Left(4)) =', S.map(x => x * x, S.Left(4)))
 
+console.log('')
+console.log('                                       APPLY                             ')
+console.log('')
+console.log("---------------------------------  the ap method -----------------------------------------")
+console.log("-> Array")
+console.log("S.ap([x => x+1, x => x*x], [1,2,3,4]) =", S.ap([x => x+1, x => x*x], [1,2,3,4]));
+console.log("-> Object")
+console.log('S.ap({a: x => S.parseInt(16)(x).value }, {a: "3", b: "4", c: "6"}) =', S.ap({a: x => S.parseInt(16)(x).value }, {a: "3", b: "4", c: "6"}));
+console.log('S.ap({x: Math.sqrt, y: S.add(1), z: S.sub(1)}, {w: 4, x: 4, y: 4}) =', S.ap({x: Math.sqrt, y: S.add(1), z: S.sub(1)}, {w: 4, x: 4, y: 4}));
+console.log("-> Function (two parameters!)")
+console.log("S.ap(s => n => s.slice(0, n)) (s => s.length/2) ('Haskell') = ", S.ap(s => n => s.slice(0, n)) (s => s.length/2) ('Haskell'));
+console.log("S.ap(s => a => a.map((x, idx) => x + idx)) (s => s.split('')) ('Haskell') =", S.ap(s => a => a.map((x, idx) => x + idx)) (s => s.split('')) ('Haskell'))
+console.log('S.ap(s => a => a.split("")) (s => s + "test") (3) =', S.ap(s => a => a.split("")) (s => s + "test") (3));
+console.log("the input value ('Haskell') is passed to the second function, the output of which is passed to the first function")
+console.log("-> Maybe")
+console.log('S.ap(S.Just(x => x * x), S.Just(20)) =', S.ap(S.Just(x => x * x), S.Just(20)));
+console.log("-> Either")
+console.log('S.ap(S.Right(Math.sqrt), S.Right(16))); =', S.ap(S.Right(Math.sqrt), S.Right(16)));
+console.log("-------------------------------  lifting a function ---------------------------------------")
+console.log('-> After lifting a function with lift2, it can be used on any 2 Applys (i.e. Maybe, Either or Array)')
 console.log('S.lift2(S.add, S.Just(3), S.Just(2)) =', S.lift2(S.add, S.Just(3), S.Just(2)))
+console.log('-> As comparison, the normal use of S.add')
+console.log('S.add(2, 3) =', S.add(2, 3))
+console.log('-> Examples of using lift2 with Arrays')
+console.log('S.lift2(S.add, [1], [5]) =', S.lift2(S.add, [1], [5]))
+console.log('S.lift2(S.add, [1,2], [5,9]) =', S.lift2(S.add, [1,2], [5,9]))
+console.log("-------------------------------------------------------------------------------------------")
+console.log('')
 console.log("S.Either['@@type'] =", S.Either['@@type'])
 
 console.log('S.alt({a:2, b: 3}, {b:6}) =', S.alt({a:2, b: 3}, {b:6}))
 
+console.log(S.of(Array, 3))
