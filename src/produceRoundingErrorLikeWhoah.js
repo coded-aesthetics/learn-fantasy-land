6f8bd4bdb8b0406a267afda52db83462e5565b5c
Jan Reinsch <jan.reinsch@kardex.com>
2018-10-17 11:21:33 +0200

    ..

--- src/produceRoundingErrorLikeWhoah.js
+++ src/produceRoundingErrorLikeWhoah.js
@@ -11,10 +11,10 @@ let f = (x) => {
   
   let cryTatchSqrt = R.tryCatch(x => S.Right(f(x)), e => S.Left(e.message))
   
-  let produceRoundingErrorLikeWhoah = R.pipe(
+  let produceRoundingErrorLikeWhoah = S.pipe([
     cryTatchSqrt,
     S.map(x => Math.pow(x, 4)),
-    S.map(x => x - 1)
+    S.map(x => x - 1)]
   );
   
   console.log(produceRoundingErrorLikeWhoah(15));
@@ -22,3 +22,12 @@ let f = (x) => {
 
   console.log(S.encase(f, 3));
   console.log(S.encase(f, -1));
+
+  let produceRoundingErrorLikeWhoah2 = S.pipe([
+    S.encaseEither(S.prop('message'), f),
+    S.map(x => Math.pow(x, 4)),
+    S.map(x => x - 1)]
+  );
+  
+  console.log(produceRoundingErrorLikeWhoah2(3));
+  console.log(produceRoundingErrorLikeWhoah2(-1));
